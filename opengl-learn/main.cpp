77d0f41ee7f671ceeb804c91d5417c6c417da110
Levi Zhang <zhanghuayu.dev@gmail.com>
2022-12-26 19:40:14 +0000

    enhanced hello

--- opengl-learn/main.cpp
+++ opengl-learn/main.cpp
@@ -1,27 +1,34 @@
 // ingnore xcode documentation warnings
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
-//ingnore deprecation warning
+// ingnore deprecation warning
 #define GL_SILENCE_DEPRECATION
+// multi-header warning for macos sdl2
+#ifdef __APPLE__
+#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#endif
 
 #define GLEW_STATIC
 #include <GL/glew.h>
-
 #define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
-
-
 #define BUFFER_OFFSET(i) ((char *)NULL (i))
+#include <iostream>
+
+void init(GLFWwindow *window){
+    
+}
+
+void display(GLFWwindow *window, double currentTime){
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main(int argc, char** argv) {
-    GLFWwindow *window;
-    
     glewExperimental = GL_TRUE;
-    
     if (!glfwInit()) {
-        return -1;
+        exit(EXIT_FAILURE);
     }
-    
     // force using newer version of OpenGL in MacOS
 #ifdef __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -31,7 +38,9 @@ int main(int argc, char** argv) {
 #endif
     
     // create window
-    window = glfwCreateWindow(1024, 768, "Hello World", NULL, NULL);
+    GLFWwindow *window;
+    window = glfwCreateWindow(600, 600, "Hello World", NULL, NULL);
+    glfwMakeContextCurrent(window);
     
     // set render resolution for Retina display
     // (make sure content rendered into frame buffer is fit with content displayed
@@ -39,21 +48,29 @@ int main(int argc, char** argv) {
     int actualScreenWidth, actualScreenHeight;
     // retrieve actual resolution from frame buffer
     glfwGetFramebufferSize(window, &actualScreenWidth, &actualScreenHeight);
+    glViewport(0,0,actualScreenWidth,actualScreenHeight);
+    
+    if (glewInit() != GLEW_OK){
+        exit(EXIT_FAILURE);
+    }
+    
+    glfwSwapInterval(1); //Vsync
     
     if (!window) {
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
     
-    glfwMakeContextCurrent(window);
-    glViewport(0,0,actualScreenWidth,actualScreenHeight);
+    init(window);
     
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glfwSwapBuffers(window);
+//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window); //Vsync
         glfwPollEvents();
     }
     
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    exit(EXIT_SUCCESS);
 }
