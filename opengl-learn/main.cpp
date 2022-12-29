a34f39828480d16f98486a672328887b66abfa11
Huayu Zhang <zhanghuayu.dev@gmail.com>
2022-12-29 16:12:35 +0000

    1

--- opengl-learn/main.cpp
+++ opengl-learn/main.cpp
@@ -1,13 +1,3 @@
-// ignore xcode documentation warnings
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdocumentation"
-// ignore deprecation warning
-#define GL_SILENCE_DEPRECATION
-// multi-header warning for MacOS sdl2
-#ifdef __APPLE__
-#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
-#endif
-
 #define GLEW_STATIC
 #include <GL/glew.h>
 #define GLFW_INCLUDE_GLCOREARB
@@ -15,91 +5,28 @@
 #define BUFFER_OFFSET(i) ((char *)NULL (i))
 #include <iostream>
 
-#define numVAOs 1
-
-GLuint renderingProgram;
-GLuint vao[numVAOs];
-
-GLuint createShaderProgram(){
-    const char *vshaderSource =
-    "#version 410 \n"
-    "void main(void) \n"
-    "{ gl_Position = vec4 (0.0, 0.0, 0.0, 1.0); }";
-
-    const char *fshaderSource =
-    "#version 410 \n"
-    "out vec4 color; \n"
-    "void main(void) \n"
-    "{ color = vec4(1.0, 0.0, 0.0, 1.0); }";
-
-    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-    glShaderSource(vShader, 1, &vshaderSource, NULL);
-    glShaderSource(fShader, 1, &fshaderSource, NULL);
-    glCompileShader(vShader);
-    glCompileShader(fShader);
-
-    GLuint vfProgram = glCreateProgram();
-    glAttachShader(vfProgram, vShader);
-    glAttachShader(vfProgram, fShader);
-    glLinkProgram(vfProgram);
-
-    return vfProgram;
-}
-
-void init(GLFWwindow *window){
-    renderingProgram = createShaderProgram();
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
-}
-
-void display(GLFWwindow *window, double currentTime){
-    glUseProgram(renderingProgram);
-    glDrawArrays(GL_POINTS, 0, 1);
-}
 
 int main(int argc, char** argv) {
     glewExperimental = GL_TRUE;
     if (!glfwInit()) {
         exit(EXIT_FAILURE);
     }
-    // force using newer version of OpenGL in MacOS
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1); // highest for MacOS
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
     
     // create window
     GLFWwindow *window;
     window = glfwCreateWindow(600, 600, "Hello World", NULL, NULL);
     glfwMakeContextCurrent(window);
-    
-    // set render resolution for Retina display
-    // (make sure content rendered into frame buffer is fit with content displayed
-    // in GLFW window)
-    int actualScreenWidth, actualScreenHeight;
-    // retrieve actual resolution from frame buffer
-    glfwGetFramebufferSize(window, &actualScreenWidth, &actualScreenHeight);
-    glViewport(0,0,actualScreenWidth,actualScreenHeight);
-    
+
     if (glewInit() != GLEW_OK){
         exit(EXIT_FAILURE);
     }
-    
-    glfwSwapInterval(1); //Vsync
-    
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    
-    init(window);
+
     
     while (!glfwWindowShouldClose(window)) {
-        display(window, glfwGetTime());
+
+
+
+        glfwSwapInterval(1); //Vsync
         glfwSwapBuffers(window); //Vsync
         glfwPollEvents();
     }
